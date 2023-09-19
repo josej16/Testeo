db641f70558df227e91a8e03fa279be4c701d692
Jose J <josebrox16@gmail.com>
2023-09-19 18:14:53 -0400

    Add files via upload

--- streamlit_app2.py
+++ streamlit_app2.py
@@ -269,8 +269,8 @@ def main():
         # Cantidad de conversaciones
         cantidad_conversaciones = len(df_oferta_snackys.loc[(df_oferta_snackys["journeyStep"] == "RespuestaMensajeInicial")].reset_index())
         # motivos_clientes_no_interesados
-        motivos_clientes_no_interesados1 = len(df_oferta_snackys.loc[(df_oferta_snackys["journeyStep"] == "RespuestaMotivoClienteParaNoSuscripcion")].reset_index()) 
-        motivos_clientes_no_interesados = f"{motivos_clientes_no_interesados1} de {subs['No suscriptos']}" 
+        con_motivo_no_interesados1 = len(df_oferta_snackys.loc[(df_oferta_snackys["journeyStep"] == "RespuestaMotivoClienteParaNoSuscripcion")].reset_index()) 
+        con_motivo_no_interesados = f"{con_motivo_no_interesados1} de {subs['No suscriptos']}" 
         # Intencion de no dejar motivos
         sin_motivo_no_interesado = len((df_oferta_snackys['msgBody'] == 0) & (df_oferta_snackys['journeyStep'] == 'RespuestaClienteQuiereDejarMotivo'))
         # Conversaciones terminadas
@@ -301,35 +301,186 @@ def main():
             }
         </style>
         """
+        custom_css2 = """ 
+        <style>
+            .ag-format-container {
+            width: 1142px;
+            margin: 0 auto;
+            }
+
+
+            body {
+            background-color: #000;
+            }
+            .ag-courses_box {
+            display: -webkit-box;
+            display: -ms-flexbox;
+            display: flex;
+            -webkit-box-align: start;
+            -ms-flex-align: start;
+            align-items: flex-start;
+            -ms-flex-wrap: wrap;
+            flex-wrap: wrap;
+
+            padding: 50px 0;
+            }
+            .ag-courses_item {
+            -ms-flex-preferred-size: calc(33.33333% - 30px);
+            flex-basis: calc(33.33333% - 30px);
+
+            margin: 0 15px 30px;
+
+            overflow: hidden;
+
+            border-radius: 28px;
+            }
+            .ag-courses-item_link {
+            display: block;
+            padding: 30px 20px;
+            background-color: #121212;
+
+            overflow: hidden;
+
+            position: relative;
+            }
+            .ag-courses-item_link:hover,
+            .ag-courses-item_link:hover .ag-courses-item_date {
+            text-decoration: none;
+            color: #FFF;
+            }
+            .ag-courses-item_link:hover .ag-courses-item_bg {
+            -webkit-transform: scale(10);
+            -ms-transform: scale(10);
+            transform: scale(10);
+            }
+            .ag-courses-item_title {
+            min-height: 87px;
+            margin: 0 0 25px;
+
+            overflow: hidden;
+
+            font-weight: bold;
+            font-size: 30px;
+            color: #FFF;
+
+            z-index: 2;
+            position: relative;
+            }
+            .ag-courses-item_date-box {
+            font-size: 18px;
+            color: #FFF;
+
+            z-index: 2;
+            position: relative;
+            }
+            .ag-courses-item_date {
+            font-weight: bold;
+            color: #f9b234;
+
+            -webkit-transition: color .5s ease;
+            -o-transition: color .5s ease;
+            transition: color .5s ease
+            }
+            .ag-courses-item_bg {
+            height: 128px;
+            width: 128px;
+            background-color: #f9b234;
+
+            z-index: 1;
+            position: absolute;
+            top: -75px;
+            right: -75px;
+
+            border-radius: 50%;
+
+            -webkit-transition: all .5s ease;
+            -o-transition: all .5s ease;
+            transition: all .5s ease;
+            }
+            .ag-courses_item:nth-child(2n) .ag-courses-item_bg {
+            background-color: #3ecd5e;
+            }
+            .ag-courses_item:nth-child(3n) .ag-courses-item_bg {
+            background-color: #e44002;
+            }
+            .ag-courses_item:nth-child(4n) .ag-courses-item_bg {
+            background-color: #952aff;
+            }
+            .ag-courses_item:nth-child(5n) .ag-courses-item_bg {
+            background-color: #cd3e94;
+            }
+            .ag-courses_item:nth-child(6n) .ag-courses-item_bg {
+            background-color: #4c49ea;
+            }
+
+
+
+            @media only screen and (max-width: 979px) {
+            .ag-courses_item {
+                -ms-flex-preferred-size: calc(50% - 30px);
+                flex-basis: calc(50% - 30px);
+            }
+            .ag-courses-item_title {
+                font-size: 24px;
+            }
+            }
+
+            @media only screen and (max-width: 767px) {
+            .ag-format-container {
+                width: 96%;
+            }
+
+            }
+            @media only screen and (max-width: 639px) {
+            .ag-courses_item {
+                -ms-flex-preferred-size: 100%;
+                flex-basis: 100%;
+            }
+            .ag-courses-item_title {
+                min-height: 72px;
+                line-height: 1;
+
+                font-size: 24px;
+            }
+            .ag-courses-item_link {
+                padding: 22px 40px;
+            }
+            .ag-courses-item_date-box {
+                font-size: 16px;
+            }
+            }
+        </style>
+        """
+
         # Agregar el estilo CSS personalizado utilizando st.markdown
-        st.markdown(custom_css, unsafe_allow_html=True)
+        st.markdown(custom_css2, unsafe_allow_html=True)
     
         # Variable de ejemplo con estilos en línea
-        tarjeta1 = f'<div class="tarjeta" style="font-size: 30px; color: #00008B;">{motivos_clientes_no_interesados}</div>'
-        tarjeta2 = f'<div class="tarjeta" style="font-size: 30px; color: #00008B;">{clientes_suscriptos}</div>'
-        tarjeta3 = f'<div class="tarjeta" style="font-size: 30px; color: #00008B;">{clientes_dejar_de_recibir}</div>'
-        tarjeta4 = f'<div class="tarjeta" style="font-size: 30px; color: #00008B;">{sin_motivo_no_interesado}</div>'
+        tarjeta1 = f'<div class="ag-courses_item" style="font-size: 30px; color: #00008B;">{con_motivo_no_interesados}</div>'
+        tarjeta2 = f'<div class="ag-courses_item" style="font-size: 30px; color: #00008B;">{clientes_suscriptos}</div>'
+        tarjeta3 = f'<div class="ag-courses_item" style="font-size: 30px; color: #00008B;">{clientes_dejar_de_recibir}</div>'
+        tarjeta4 = f'<div class="ag-courses_item" style="font-size: 30px; color: #00008B;">{sin_motivo_no_interesado}</div>'
 
         # Contenido de las tarjetas
         with col1:
-            st.markdown('<div class="subheader">Cantidad de clientes que dejaron motivos</div>', unsafe_allow_html=True)
+            st.markdown('<div class="ag-courses-item_title">Cantidad de clientes que dejaron motivos</div>', unsafe_allow_html=True)
             st.markdown(tarjeta1, unsafe_allow_html=True)
             st.markdown('</div>', unsafe_allow_html=True)
             # st.write(f"+ Conversaciones terminadas: **{conversaciones_terminadas}**")
             # st.write(f"+ Conversaciones incompletas: **{conversaciones_incompletas}**")
 
         with col2:
-            st.markdown('<div class="subheader">Clientes suscriptos</div>', unsafe_allow_html=True)
+            st.markdown('<div class="ag-courses-item_title">Clientes suscriptos</div>', unsafe_allow_html=True)
             st.markdown(tarjeta2, unsafe_allow_html=True)
             st.markdown('</div></div>', unsafe_allow_html=True)
 
         with col3:
-            st.markdown('<div class="subheader">Clientes que se dieron de baja</div>', unsafe_allow_html=True)
+            st.markdown('<div class="ag-courses-item_title">Clientes que se dieron de baja</div>', unsafe_allow_html=True)
             st.markdown(tarjeta3, unsafe_allow_html=True)
             st.markdown('</div></div>', unsafe_allow_html=True)
 
         with col4:
-            st.markdown('<div class="subheader">Clientes que no dejaron motivos</div>', unsafe_allow_html=True)
+            st.markdown('<div class="ag-courses-item_title">Clientes que no dejaron motivos</div>', unsafe_allow_html=True)
             st.markdown(tarjeta4, unsafe_allow_html=True)
             st.markdown('</div></div>', unsafe_allow_html=True)
             # ver_motivos = st.checkbox("Mostrar motivos")
@@ -398,8 +549,8 @@ def main():
 
     # Snackys
     opciones_paginas_snackys = ["Ofertas", "Recompra"]
-    pagina_seleccionada = st.sidebar.selectbox("Selecciona una página:", opciones_paginas_snackys)
-
+    # pagina_seleccionada = st.sidebar.selectbox("Selecciona una página:", opciones_paginas_snackys)
+    pagina_seleccionada = st.selectbox("Selecciona una página:", opciones_paginas_snackys)
     # Mostrar páginas para Snackys
     # if pagina_seleccionada == "Inicio":
     #     pagina_inicio()
